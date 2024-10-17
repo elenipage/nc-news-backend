d42cd52bdcfe4a044545ab5e0fcedc4b0886aaa1
elenipage <elenipage@icloud.com>
2024-10-17 16:32:00 +0100

    amend connection file

--- db/connection.js
+++ db/connection.js
@@ -6,13 +6,11 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 if (ENV === "production") {
-  if (!process.env.DATABASE_URL) {throw new Error('DATABASE_URL not set')}
-  
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
